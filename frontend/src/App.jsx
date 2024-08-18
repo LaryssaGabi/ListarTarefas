aac4ac43bbb7798785a1e2b7b225d4fe50f9b7d3
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-18 18:31:16 -0300

    projeto iniacial listar tarefas

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="O que eu tenho que fazer..." />
+        <button>Adicionar</button>
+
+        <ul>
+          <li>Levar o Nico para passear</li>
+          <li>Levar o Nico para passear</li>
+          <li>Levar o Nico para passear</li>
+        </ul>
+
+      </div>
+    </>
+  )
+}
+
+
